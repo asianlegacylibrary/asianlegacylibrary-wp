5dbaa3076b24c425c97635c93f3093d21a2c8057
Drasko Stikovac <drasko@eutelnet.com>
2021-10-14 17:42:08 +0200

    Added google tag manager

--- header.php
+++ header.php
@@ -25,10 +25,23 @@
     <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 	<script type="text/javascript">var ajaxurl = "<?php echo admin_url('admin-ajax.php'); ?>";</script>
 	<?php wp_head(); ?>
+	
+	<!-- Google Tag Manager -->
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-M7XFXCC');</script>
+	<!-- End Google Tag Manager -->
 
 </head>
 
 <body <?php body_class(); ?>>
+	
+	<!-- Google Tag Manager (noscript) -->
+	<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M7XFXCC"
+	height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+	<!-- End Google Tag Manager (noscript) -->
 	
 	<div class="loading-holder"></div>
 	<div class="landscape-message"></div>
