75b38d8f89c3c1769eecfc610217bcc3044d7774
Drasko Stikovac <drasko@eutelnet.com>
2021-11-06 19:05:58 +0100

    Update item-news.php

--- partials/item-news.php
+++ partials/item-news.php
@@ -3,7 +3,7 @@
 		<?php $featured_img_url = get_the_post_thumbnail_url(get_the_ID(),'large'); ?>
 
 		<figure <?php if ( has_post_thumbnail() ) { ?> class="has-image" <?php } ?>>
-			<a href="<?php the_permalink(); ?>" <?php if ( has_post_thumbnail() ) { ?>style="background-image:url(<?php echo $featured_img_url; ?>);" <?php } ?>></a>
+			<a href="<?php the_permalink(); ?>" <?php if ( has_post_thumbnail() ) { ?> class="inner-post-thumbnail" style="background-image:url(<?php echo $featured_img_url; ?>);" <?php } ?>></a>
 		</figure>
 		<small class="meta"><?php the_author(); ?> - <?php the_time('jS F Y'); ?></small>
 		<h3><a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a></h3>
