47265019b38e766469986afef845b51f002c7ef8
Drasko Stikovac <drasko@eutelnet.com>
2022-03-28 15:35:40 +0200

    Added social widgets

--- sidebar.php
+++ sidebar.php
@@ -83,3 +83,13 @@ if (!is_active_sidebar('sidebar-1')) {
 		?>
 
 	</div>
+
+    <div class="widget insta">
+        <h3>Instagram</h3>
+        <?php echo do_shortcode('[instagram-feed feed=1]'); ?>
+    </div>
+
+    <div class="widget facebook">
+        <h3>Facebook</h3>
+        <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fasianlegacylibrary&tabs=timeline&width=340&height=500&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=false&appId=253092448037397" width="100%" height="500" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
+    </div>
