20d1a768e678926a61bc75241b331eeeccbad7f0
Drasko Stikovac <drasko@eutelnet.com>
2022-04-14 11:48:59 +0200

    Small update

--- inits/acf.php
+++ inits/acf.php
@@ -5,6 +5,24 @@
  * @package all_wp_theme
  */
 
+
+    // ACF title change
+	function my_layout_title($title, $field, $layout, $i) {
+		if($value = get_sub_field('title')) {
+			return $value;
+		} else {
+			foreach($layout['sub_fields'] as $sub) {
+				if($sub['name'] == 'title') {
+					$key = $sub['key'];
+					if(array_key_exists($i, $field['value']) && $value = $field['value'][$i][$key])
+						return $value;
+				}
+			}
+		}
+		return $title;
+	}
+	add_filter('acf/fields/flexible_content/layout_title', 'my_layout_title', 10, 4);
+
 	/* OPTIONS PAGES */
 	if( function_exists('acf_add_options_page') ) {
 
