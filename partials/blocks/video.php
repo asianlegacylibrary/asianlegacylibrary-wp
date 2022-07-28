d9594c78a48cd2682f32d46412ec52f01a8d400c
joelcrawford <joelrootcrawford@gmail.com>
2022-07-28 15:42:49 -0700

    updates to video block to create iframe from URL instead of having user copy in embed code

--- partials/blocks/video.php
+++ partials/blocks/video.php
@@ -19,7 +19,22 @@ $video_file 				= get_field('video_file');
 $poster_image 				= get_field('poster_image');
 $video_caption 				= get_field('video_caption');
 $external_video 			= get_field('external_video');
+
+# add new ACF for video URL, afterwards we can delete some of these unused ---------------------
+$external_video_url 		= get_field('external_video_url');
+
 $external_video_platform 	= get_field('external_video_platform');
+
+# Note: need to update how translators can 'translate' the video URL for their language -----------------------
+# external_video_url will be the video URL, instead of using the embed code (iframe)
+# get the ID of the video from the URL (last element)
+$video_url_bits = explode('/', $external_video_url);
+$video_id = end($video_url_bits);
+$video_url = "https://player.vimeo.com/video/". $video_id . "?";
+echo var_dump('video_url', $video_url);
+
+# we replace the $external_video (which was iframe) with our iframe with the video_url 
+
 
 ?>
 
@@ -28,7 +43,8 @@ $external_video_platform 	= get_field('external_video_platform');
 		<?php if($external_video) : ?>
 		<div class="video">
 			<div class="external-video">
-				<?php echo $external_video; ?>
+				<!--<?php #echo $external_video; ?> -->
+				<iframe src="<?php echo $video_url; ?>" width="640" height="360" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
 			</div>
 			<span class="with-effect" style="background-image: url(<?php echo $poster_image['url']; ?>)"><span class="play"></span></span>
 		</div>
